6fbc776e402661220fbb43a20092c8fe9409b7f7
Samuel-Saturno <111779961+Samuel-Saturno@users.noreply.github.com>
2025-10-20 13:51:16 -0300

    Add of the rigth Spring Boot file
    This commit have the base struture for the cod

---
+++ loja-variedades/loja-variedades/src/main/java/com/loja/loja_variedades/LojaVariedadesApplication.java
@@ -0,0 +1,13 @@
+package com.loja.loja_variedades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVariedadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVariedadesApplication.class, args);
+	}
+
+}
